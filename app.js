29b86ae33fa60bf8e7f333bb3b3bf9e3fa5b6a45
IT21016684 <110347124+KanishkaGJ@users.noreply.github.com>
2023-03-04 10:00:26 +0530

    adding async awaits

--- app.js
+++ app.js
@@ -33,3 +33,10 @@ const myPromise = new Promise((resolve, reject) => { if (condition) {
     myPromise.then((result) => { console.log(result);
     }).catch((error) => { console.log(error);
     });
+
+
+    async function myFunction() { try {
+        const result = await myPromise;
+        console.log(result); } catch (error) {
+        console.log(error); }
+        } myFunction();
